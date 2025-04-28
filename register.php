b8aba1df7bd5ce80266dcbbf30098f862695f9a3
Omar Adel <omaradel.bu@gmail.com>
2025-04-29 01:26:28 +0300

    fix url

--- register.php
+++ register.php
@@ -1,5 +1,5 @@
 <?php
-include '/config/db_connection.php';
+include './config/db_connection.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $data = json_decode(file_get_contents('php://input'), true);
