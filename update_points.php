757f4171fbf1cad1341f1d2e7cda745d538feb7b
Omar Adel <omaradel.bu@gmail.com>
2025-06-13 21:14:23 +0300

    cors

--- update_points.php
+++ update_points.php
@@ -1,7 +1,13 @@
 <?php
 include './config/db_connection.php';
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: *"); // or set a specific origin
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 header('Content-Type: application/json');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
