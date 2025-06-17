00c2e6b7956f5977c6fc4feecd711feee416fe9f
Omar Adel <omaradel.bu@gmail.com>
2025-06-17 17:41:20 +0300

    test

--- waste_disposal.php
+++ waste_disposal.php
@@ -4,6 +4,11 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
+// ✅ Respond to OPTIONS preflight request
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $data = json_decode(file_get_contents("php://input"), true);
 
