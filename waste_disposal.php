f74b56bc23a06380d79ad32f558c958b4e05f4ab
Omar Adel <omaradel.bu@gmail.com>
2025-06-17 17:25:25 +0300

    cors

--- waste_disposal.php
+++ waste_disposal.php
@@ -1,6 +1,8 @@
 <?php
 include './config/db_connection.php';
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $data = json_decode(file_get_contents("php://input"), true);
